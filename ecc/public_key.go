c4328879cd2aac77bc77bbd20bc189858cefca08
regnull <regnull@gmail.com>
2021-04-26 20:53:11 -0400

    public key serialization

--- ecc/public_key.go
+++ ecc/public_key.go
@@ -2,12 +2,56 @@ package ecc
 
 import (
 	"crypto/ecdsa"
+	"fmt"
 	"math/big"
+
+	"github.com/btcsuite/btcd/btcec"
 )
 
 // PublicKey represents elliptic curve cryptography private key.
 type PublicKey struct {
 	publicKey *ecdsa.PublicKey
+}
+
+func NewPublicFromSerializedCompressed(serialized []byte) (*PublicKey, error) {
+	if len(serialized) != 33 {
+		return nil, fmt.Errorf("invalid serialized compressed public key")
+	}
+
+	even := false
+	if serialized[0] == 0x02 {
+		even = true
+	} else if serialized[0] == 0x03 {
+		even = false
+	} else {
+		return nil, fmt.Errorf("invalid serialized compressed public key")
+	}
+	x := new(big.Int).SetBytes(serialized[1:])
+	P := btcec.S256().CurveParams.P
+	sqrtExp := new(big.Int).Add(P, big.NewInt(1))
+	sqrtExp = sqrtExp.Div(sqrtExp, big.NewInt(4))
+	alpha := new(big.Int).Exp(x, big.NewInt(3), P)
+	alpha.Add(alpha, btcec.S256().B)
+	beta := new(big.Int).Exp(alpha, sqrtExp, P)
+	var evenBeta *big.Int
+	var oddBeta *big.Int
+	if new(big.Int).Div(beta, big.NewInt(2)).Cmp(big.NewInt(0)) == 0 {
+		evenBeta = beta
+		oddBeta = new(big.Int).Sub(P, beta)
+	} else {
+		evenBeta = new(big.Int).Sub(P, beta)
+		oddBeta = beta
+	}
+	var y *big.Int
+	if even {
+		y = evenBeta
+	} else {
+		y = oddBeta
+	}
+	return &PublicKey{publicKey: &ecdsa.PublicKey{
+		Curve: btcec.S256(),
+		X:     x,
+		Y:     y}}, nil
 }
 
 // SerializeCompressed returns the private key serialized in SEC compressed format.
--- ecc/public_key_test.go
+++ ecc/public_key_test.go
@@ -17,3 +17,14 @@ func Test_PublicKey_SerializeCompressed(t *testing.T) {
 	assert.EqualValues("0357a4f368868a8a6d572991e484e664810ff14c05c0fa023275251151fe0e53d1", fmt.Sprintf("%x", serialized))
 	assert.True(true)
 }
+
+func Test_PublicKey_FromSerializedCompressed(t *testing.T) {
+	assert := assert.New(t)
+
+	serialized, _ := new(big.Int).SetString("0357a4f368868a8a6d572991e484e664810ff14c05c0fa023275251151fe0e53d1", 16)
+	publicKey, err := NewPublicFromSerializedCompressed(serialized.Bytes())
+	assert.NoError(err)
+	assert.NotNil(publicKey)
+	assert.EqualValues("57a4f368868a8a6d572991e484e664810ff14c05c0fa023275251151fe0e53d1", fmt.Sprintf("%064x", publicKey.publicKey.X))
+	assert.EqualValues("0d6cc87c5bc29b83368e17869e964f2f53d52ea3aa3e5a9efa1fa578123a0c6d", fmt.Sprintf("%064x", publicKey.publicKey.Y))
+}
