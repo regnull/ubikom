4daa9b3ac12a2cef5666f4b6d8ac49cd086401ff
regnull <regnull@gmail.com>
2021-04-28 15:25:50 -0400

    generate pow

--- cmd/ubikom-cli/cmd/register.go
+++ cmd/ubikom-cli/cmd/register.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/binary"
 	"log"
 	"os"
 	"path"
@@ -10,6 +12,11 @@ import (
 	"google.golang.org/grpc"
 	"teralyt.com/ubikom/ecc"
 	"teralyt.com/ubikom/pb"
+	"teralyt.com/ubikom/util"
+)
+
+const (
+	leadingZeros = 26
 )
 
 func init() {
@@ -36,7 +43,7 @@ var registerKeyCmd = &cobra.Command{
 		}
 		dir := path.Join(homeDir, defaultHomeSubDir)
 		keyFile := path.Join(dir, defaultKeyFile)
-		publicKey, err := ecc.LoadPrivateKey(keyFile)
+		privateKey, err := ecc.LoadPrivateKey(keyFile)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -48,10 +55,17 @@ var registerKeyCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 		defer conn.Close()
+
+		compressedKey := privateKey.PublicKey().SerializeCompressed()
+		log.Printf("generating POW...")
+		pow := generatePOW(compressedKey, leadingZeros)
+		log.Printf("POW found: %d", pow)
+
 		client := pb.NewIdentityServiceClient(conn)
 		res, err := client.RegisterKey(context.TODO(),
 			&pb.KeyRegistrationRequest{
-				Key: publicKey.PublicKey().SerializeCompressed(),
+				Key: compressedKey,
+				Pow: pow,
 			})
 		if err != nil {
 			log.Fatal(err)
@@ -61,4 +75,21 @@ var registerKeyCmd = &cobra.Command{
 		}
 		log.Printf("key registered successfully")
 	},
+}
+
+func generatePOW(data []byte, leadingZeros int) int32 {
+	powBuf := make([]byte, 4)
+	buf := make([]byte, len(data)+4)
+	pow := int32(555)
+	for {
+		binary.BigEndian.PutUint32(powBuf, uint32(pow))
+		for i := 0; i < 4; i++ {
+			buf[len(data)+i] = powBuf[i]
+		}
+		h := sha256.Sum256(buf)
+		if util.VerifyPOW(h[:], leadingZeros) {
+			return pow
+		}
+		pow++
+	}
 }
