f592f91c42437fec14876a04624aacc24b9194d6
regnull <regnull@gmail.com>
2021-04-29 17:53:55 -0400

    initialize zerolog

--- cmd/ubikom-server/main.go
+++ cmd/ubikom-server/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"path"
 
 	"github.com/dgraph-io/badger/v3"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 	"teralyt.com/ubikom/pb"
 	"teralyt.com/ubikom/server"
@@ -26,6 +27,9 @@ type CmdArgs struct {
 }
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "15:04:05"})
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
 	var args CmdArgs
 	flag.IntVar(&args.Port, "port", defaultPort, "port to listen to")
 	flag.StringVar(&args.BaseDir, "base-dir", "", "base directory")
@@ -33,23 +37,23 @@ func main() {
 
 	dbDir, err := getDBDir(args.BaseDir)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("failed to get database directory")
 	}
 	db, err := badger.Open(badger.DefaultOptions(dbDir))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("failed to initialize the database")
 	}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", args.Port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatal().Err(err).Msg("failed to listen")
 	}
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 	srv := server.NewServer(db)
 	pb.RegisterIdentityServiceServer(grpcServer, srv)
 	pb.RegisterLookupServiceServer(grpcServer, srv)
-	log.Printf("listening on port %d...", args.Port)
+	log.Info().Int("port", args.Port).Msg("server is up and running")
 	grpcServer.Serve(lis)
 }
 
