c1f46503a61187efcef87886598e89ad8ebbd704
regnull <regnull@gmail.com>
2021-05-05 16:40:02 -0400

    build script

---
+++ globals/globals.go
@@ -0,0 +1,7 @@
+package globals
+
+const (
+	PublicIdentityServiceURL = "alpha.ubikom.cc:8825"
+	PublicLookupServiceURL   = "alpha.ubikom.cc:8825"
+	PublicDumpServiceURL     = "alpha.ubikom.cc:8826"
+)
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,15 +2,21 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 TEMP_DIR=$(mktemp -d)
+GOOS="linux darwin windows"
+GOARCH="amd64"
 for BIN_NAME in ubikom-server ubikom-dump ubikom-cli ubikom-proxy
 do
     MAIN_DIR="$SCRIPT_DIR/../cmd/$BIN_NAME"
     pushd $MAIN_DIR > /dev/null
-    echo GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GO_EXTLINK_ENABLED=0 go build -v -o $TEMP_DIR/$BIN_NAME-linux $MAIN_DIR/main.go
-    GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GO_EXTLINK_ENABLED=0 go build -v -o $TEMP_DIR/$BIN_NAME-linux $MAIN_DIR/main.go
-    go build -v -o $TEMP_DIR/$BIN_NAME-darwin $MAIN_DIR/main.go
-    cp $TEMP_DIR/$BIN_NAME-linux $SCRIPT_DIR/../build/
-    cp $TEMP_DIR/$BIN_NAME-darwin $SCRIPT_DIR/../build/
+    for OS in $GOOS
+    do
+        for ARCH in $GOARCH
+        do
+            GOOS=$OS GOARCH=$ARCH CGO_ENABLED=0 GO_EXTLINK_ENABLED=0 go build -v -o $TEMP_DIR/$BIN_NAME-$OS-$ARCH $MAIN_DIR/main.go
+            mkdir $SCRIPT_DIR/../build/$OS-$ARCH > /dev/null 2>&1
+            cp $TEMP_DIR/$BIN_NAME-$OS-$ARCH $SCRIPT_DIR/../build/$OS-$ARCH/$BIN_NAME
+        done
+    done
     popd > /dev/null
 done
 rm -rf $TEMP_DIR
