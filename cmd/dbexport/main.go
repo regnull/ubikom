151f1803662d3eed2db97de6da6f5d61c4cf8ac2
regnull <regnull@gmail.com>
2021-07-14 16:30:41 -0400

    generate snapshot signature

--- cmd/dbexport/main.go
+++ cmd/dbexport/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"io/ioutil"
 	"math"
 	"os"
 
@@ -9,13 +11,39 @@ import (
 	"github.com/regnull/easyecc"
 	"github.com/regnull/ubikom/db"
 	"github.com/regnull/ubikom/protoio"
+	"github.com/regnull/ubikom/protoutil"
+	"github.com/regnull/ubikom/util"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"google.golang.org/protobuf/proto"
 )
+
+type CmdArgs struct {
+	KeyLocation string
+}
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "15:04:05"})
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+
+	var args CmdArgs
+	flag.StringVar(&args.KeyLocation, "key", "", "key location")
+	flag.Parse()
+
+	keyFile := args.KeyLocation
+	var err error
+	if keyFile == "" {
+		keyFile, err = util.GetDefaultKeyLocation()
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get private key")
+		}
+	}
+	key, err := easyecc.NewPrivateKeyFromFile(keyFile, "")
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to load private key")
+	}
+	log.Info().Str("file", keyFile).Msg("private key loaded")
+
 	badger, err := badger.Open(badger.DefaultOptions("db"))
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to initialize the database")
@@ -38,4 +66,17 @@ func main() {
 	}
 	w.Flush()
 	f.Close()
+
+	signed, err := protoutil.CreateSigned(key, hashWriter.Hash())
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to create signature")
+	}
+	b, err := proto.Marshal(signed)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to marshal signature")
+	}
+	err = ioutil.WriteFile("snapshot_sig", b, 0440)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to write snapshot signature")
+	}
 }
