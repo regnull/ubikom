93dc45a80ad96a503f7e41094f6b93fce8a85dfc
regnull <regnull@gmail.com>
2021-11-09 13:16:18 -0500

    get mailboxes test

--- imap/db/badger_test.go
+++ imap/db/badger_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -22,6 +23,35 @@ func Test_CreateGetMailbox(t *testing.T) {
 	mb, err = b.GetMailbox("foo", "bar")
 	assert.NotNil(mb)
 	assert.NoError(err)
+	assert.EqualValues("bar", mb.GetName())
+}
+
+func Test_GetMailboxes(t *testing.T) {
+	assert := assert.New(t)
+
+	b, cleanup, err := createTestBadgerStore()
+	assert.NoError(err)
+	defer cleanup()
+
+	assert.NoError(b.CreateMailbox("foo", "mb1"))
+	assert.NoError(b.CreateMailbox("foo", "mb2"))
+	assert.NoError(b.CreateMailbox("foo", "mb3"))
+
+	mbs, err := b.GetMailboxes("foo")
+	assert.NoError(err)
+	assert.EqualValues(3, len(mbs))
+
+	for i := 1; i <= 3; i++ {
+		name := fmt.Sprintf("mb%d", i)
+		found := false
+		for _, mb := range mbs {
+			if mb.GetName() == name {
+				found = true
+				break
+			}
+		}
+		assert.True(found)
+	}
 }
 
 func createTestBadgerStore() (*Badger, func(), error) {
