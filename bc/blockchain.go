4932d1524c18d9afb958cada53ea092a14da493c
regnull <regnull@gmail.com>
2022-01-10 20:09:44 -0500

    blockchain fixes

--- bc/blockchain.go
+++ bc/blockchain.go
@@ -3,6 +3,7 @@ package bc
 import (
 	"context"
 	"errors"
+	"fmt"
 	"math/big"
 	"time"
 
@@ -213,13 +214,13 @@ func (b *Blockchain) WaitForConfirmation(ctx context.Context, tx string) (uint64
 
 func (b *Blockchain) MaybeRegisterUser(ctx context.Context, name, password string) error {
 	log.Info().Str("user", name).Msg("checking user blockchain registration")
-	nameRegCaller, err := gocontract.NewNameRegistry(b.connectorRegistryContractAddress, b.client)
+	nameRegCaller, err := gocontract.NewNameRegistry(b.nameRegistryContractAddres, b.client)
 	if err != nil {
-		return err
+		return fmt.Errorf("error getting name registry on blockchain: %w", err)
 	}
 	key, err := nameRegCaller.GetKey(nil, name)
 	if err != nil {
-		return err
+		return fmt.Errorf("error getting key on blockchain: %w", err)
 	}
 	if len(key) == 33 {
 		log.Debug().Str("user", name).Msg("name is already registered")
@@ -229,17 +230,17 @@ func (b *Blockchain) MaybeRegisterUser(ctx context.Context, name, password strin
 	privateKey := util.GenerateCanonicalKeyFromNamePassword(name, password)
 	keyTx, err := b.RegisterKey(ctx, privateKey.PublicKey())
 	if err != nil {
-		return err
+		return fmt.Errorf("error registering key on blockchain: %w", err)
 	}
 	log.Debug().Str("tx", keyTx).Msg("key is registered")
 	nameTx, err := b.RegisterName(ctx, privateKey.PublicKey(), name)
 	if err != nil {
-		return err
+		return fmt.Errorf("error registering name on blockchain: %w", err)
 	}
 	log.Debug().Str("tx", nameTx).Msg("name is registered")
 	connectorTx, err := b.RegisterConnector(ctx, name, "PL_DMS", globals.PublicDumpServiceURL)
 	if err != nil {
-		return err
+		return fmt.Errorf("error registering connector on blockchain: %w", err)
 	}
 	log.Debug().Str("tx", connectorTx).Msg("connector is registered")
 
@@ -248,17 +249,17 @@ func (b *Blockchain) MaybeRegisterUser(ctx context.Context, name, password strin
 
 	block, err := b.WaitForConfirmation(ctx1, keyTx)
 	if err != nil {
-		return err
+		return fmt.Errorf("error waiting for blockchain confirmation: %w", err)
 	}
 	log.Debug().Str("tx", keyTx).Uint64("block", block).Msg("key registration tx confirmed")
 	block, err = b.WaitForConfirmation(ctx1, nameTx)
 	if err != nil {
-		return err
+		return fmt.Errorf("error waiting for blockchain confirmation: %w", err)
 	}
 	log.Debug().Str("tx", nameTx).Uint64("block", block).Msg("name registration tx confirmed")
 	block, err = b.WaitForConfirmation(ctx1, connectorTx)
 	if err != nil {
-		return err
+		return fmt.Errorf("error waiting for blockchain confirmation: %w", err)
 	}
 	log.Debug().Str("tx", connectorTx).Uint64("block", block).Msg("connector registration tx confirmed")
 	return nil
