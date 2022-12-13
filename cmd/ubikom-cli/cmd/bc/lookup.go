93b00942ad9d8a2c1d25aefe402f51466922023e
Leonid Gorkin <regnull@gmail.com>
2022-12-12 20:56:31 -0500

    wip

--- cmd/ubikom-cli/cmd/bc/lookup.go
+++ cmd/ubikom-cli/cmd/bc/lookup.go
@@ -1,30 +1,26 @@
 package bc
 
 import (
-	"encoding/hex"
+	"bytes"
 	"fmt"
+	"math/big"
 
-	"github.com/btcsuite/btcutil/base58"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/regnull/easyecc"
 	"github.com/regnull/ubchain/gocontract"
+	cntv2 "github.com/regnull/ubchain/gocontract/v2"
 	"github.com/regnull/ubikom/globals"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
 func init() {
-	lookupKeyCmd.Flags().String("key", "", "key to look up")
-	lookupKeyCmd.Flags().String("key-hex", "", "key to look up (overrides --key)")
-	lookupKeyCmd.Flags().String("contract-address", globals.KeyRegistryContractAddress, "contract address")
-
 	lookupNameCmd.Flags().String("contract-address", globals.NameRegistryContractAddress, "contract address")
 
 	lookupConnectorCmd.Flags().String("protocol", "PL_DMS", "protocol to look up")
 	lookupConnectorCmd.Flags().String("contract-address", globals.ConnectorRegistryContractAddress, "contract address")
 
-	lookupCmd.AddCommand(lookupKeyCmd)
 	lookupCmd.AddCommand(lookupNameCmd)
 	lookupCmd.AddCommand(lookupConnectorCmd)
 
@@ -36,73 +32,6 @@ var lookupCmd = &cobra.Command{
 	Short: "Lookup stuff on the blockchain",
 	Long:  "Lookup stuff on the blockchain",
 	Run: func(cmd *cobra.Command, args []string) {
-	},
-}
-
-var lookupKeyCmd = &cobra.Command{
-	Use:   "key",
-	Short: "Get key",
-	Long:  "Get key",
-	Run: func(cmd *cobra.Command, args []string) {
-		nodeURL, err := cmd.Flags().GetString("node-url")
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to get node URL")
-		}
-
-		var publicKey []byte
-
-		keyHex, err := cmd.Flags().GetString("key-hex")
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to get hex key")
-		}
-
-		if keyHex != "" {
-			publicKey, err = hex.DecodeString(keyHex)
-			if err != nil {
-				log.Fatal().Err(err).Msg("failed to decode hex key")
-			}
-		} else {
-			key, err := LoadKeyFromFlag(cmd, "key")
-			if err != nil {
-				log.Fatal().Err(err).Msg("failed to load key")
-			}
-			publicKey = key.PublicKey().SerializeCompressed()
-		}
-
-		contractAddress, err := cmd.Flags().GetString("contract-address")
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to load contract address")
-		}
-
-		// Connect to the node.
-		client, err := ethclient.Dial(nodeURL)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to connect to blockchain node")
-		}
-
-		instance, err := gocontract.NewKeyRegistryCaller(common.HexToAddress(contractAddress), client)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to get contract instance")
-		}
-
-		registered, err := instance.Registered(nil, publicKey)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to query the key")
-		}
-
-		disabled, err := instance.Disabled(nil, publicKey)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to query the key")
-		}
-
-		owner, err := instance.Owner(nil, publicKey)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to query the key")
-		}
-
-		fmt.Printf("registered: %t\n", registered)
-		fmt.Printf("disabled: %t\n", disabled)
-		fmt.Printf("owner: %s\n", owner.Hex())
 	},
 }
 
@@ -133,30 +62,30 @@ var lookupNameCmd = &cobra.Command{
 			log.Fatal().Err(err).Msg("failed to connect to blockchain node")
 		}
 
-		instance, err := gocontract.NewNameRegistryCaller(common.HexToAddress(contractAddress), client)
+		instance, err := cntv2.NewNameRegistryCaller(common.HexToAddress(contractAddress), client)
 		if err != nil {
 			log.Fatal().Err(err).Msg("failed to get contract instance")
 		}
 
-		key, err := instance.GetKey(nil, name)
+		res, err := instance.LookupName(nil, name)
 		if err != nil {
 			log.Fatal().Err(err).Msg("failed to query the key")
 		}
 
-		if len(key) == 0 {
+		zeroAddress := common.BigToAddress(big.NewInt(0))
+		if bytes.Compare(res.Owner.Bytes(), zeroAddress.Bytes()) == 0 {
 			fmt.Printf("name is not registered\n")
 			return
 		}
 
-		publicKey, err := easyecc.NewPublicFromSerializedCompressed(key)
+		publicKey, err := easyecc.NewPublicFromSerializedCompressed(res.PublicKey)
 		if err != nil {
 			log.Fatal().Err(err).Msg("invalid key returned")
 		}
 
-		fmt.Printf("hex: %x\n", key)
-		fmt.Printf("base58: %s\n", base58.Encode(key))
-		fmt.Printf("btc addr: %s\n", publicKey.Address())
-		fmt.Printf("eth addr: %s\n", publicKey.EthereumAddress())
+		fmt.Printf("hex: %x\n", publicKey.SerializeCompressed())
+		fmt.Printf("owner: %x\n", res.Owner)
+		fmt.Printf("price: %s\n", res.Price.String())
 	},
 }
 
