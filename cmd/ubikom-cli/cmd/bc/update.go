9546c908c21a7f74eb10a3bf1818b9bf5a67132b
Leonid Gorkin <regnull@gmail.com>
2022-12-15 09:04:09 -0500

    update price test

--- cmd/ubikom-cli/cmd/bc/update.go
+++ cmd/ubikom-cli/cmd/bc/update.go
@@ -1,6 +1,8 @@
 package bc
 
 import (
+	"math/big"
+
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -20,8 +22,13 @@ func init() {
 	updateOwnerCmd.Flags().String("new-owner-address", "", "new owner address")
 	updateOwnerCmd.Flags().String("contract-address", globals.NameRegistryContractAddress, "contract address")
 
+	updatePriceCmd.Flags().String("key", "", "key to authorize the transaction")
+	updatePriceCmd.Flags().Int64("price", 0, "new price")
+	updatePriceCmd.Flags().String("contract-address", globals.NameRegistryContractAddress, "contract address")
+
 	updateCmd.AddCommand(updatePublicKeyCmd)
 	updateCmd.AddCommand(updateOwnerCmd)
+	updateCmd.AddCommand(updatePriceCmd)
 
 	BCCmd.AddCommand(updateCmd)
 }
@@ -115,6 +122,51 @@ var updateOwnerCmd = &cobra.Command{
 				}
 
 				tx, err := instance.UpdateOwnership(auth, name, newOwnerAddress)
+				if err != nil {
+					log.Fatal().Err(err).Msg("failed to register name")
+				}
+				return tx, err
+			})
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to register name")
+		}
+	},
+}
+
+var updatePriceCmd = &cobra.Command{
+	Use:   "price",
+	Short: "Update name price on the blockchain",
+	Long:  "Update name price on the blockchain",
+	Run: func(cmd *cobra.Command, args []string) {
+		key, err := LoadKeyFromFlag(cmd, "key")
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to load key")
+		}
+		if len(args) < 1 {
+			log.Fatal().Msg("name must be specified")
+		}
+		name := args[0]
+
+		nodeURL, err := cmd.Flags().GetString("node-url")
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get node URL")
+		}
+		price, err := cmd.Flags().GetInt64("price")
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get new owner address")
+		}
+		contractAddress, err := cmd.Flags().GetString("contract-address")
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to load contract address")
+		}
+		err = interactWithContract(nodeURL, key, contractAddress,
+			func(client *ethclient.Client, auth *bind.TransactOpts, addr common.Address) (*types.Transaction, error) {
+				instance, err := cntv2.NewNameRegistry(addr, client)
+				if err != nil {
+					log.Fatal().Err(err).Msg("failed to get contract instance")
+				}
+
+				tx, err := instance.UpdatePrice(auth, name, big.NewInt(price))
 				if err != nil {
 					log.Fatal().Err(err).Msg("failed to register name")
 				}
--- scripts/test_bc.sh
+++ scripts/test_bc.sh
@@ -11,15 +11,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd )"
 TEMP_DIR=$(mktemp -d)
 NODE_URL=http://127.0.0.1:7545
 
-# Create the key file.
-echo $1 | xxd -r -p - $TEMP_DIR/key
+# Create the key files.
+echo $1 | xxd -r -p - $TEMP_DIR/key1
+echo $2 | xxd -r -p - $TEMP_DIR/key2
 
 # Build ubkom-cli.
 cd $SCRIPT_DIR/../cmd/ubikom-cli
 go build -o $TEMP_DIR/ubikom-cli || exit 1
 
+# Get addresses.
+ADDRESS1=$($TEMP_DIR/ubikom-cli get ethereum-address --key=$TEMP_DIR/key1) || exit 1
+ADDRESS2=$($TEMP_DIR/ubikom-cli get ethereum-address --key=$TEMP_DIR/key2) || exit 1
+
 # Deploy the contract.
-REG_RES=$($TEMP_DIR/ubikom-cli bc deploy registry --key=$TEMP_DIR/key --node-url=$NODE_URL)
+REG_RES=$($TEMP_DIR/ubikom-cli bc deploy registry --key=$TEMP_DIR/key1 --node-url=$NODE_URL)
 if [ "$?" -ne 0 ];
 then
     exit 1
@@ -31,31 +36,24 @@ CONTRACT_ADDRESS=$(echo $REG_RES| jq -r ".Address")
 $TEMP_DIR/ubikom-cli create key --out=$TEMP_DIR/pub_key --skip-passphrase || exit 1
 
 # Try to register an invalid name.
-$TEMP_DIR/ubikom-cli bc register name "$$$" --key=$TEMP_DIR/key --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL && exit 1
-$TEMP_DIR/ubikom-cli bc register name "_" --key=$TEMP_DIR/key --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL && exit 1
-$TEMP_DIR/ubikom-cli bc register name "-foo" --key=$TEMP_DIR/key --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL && exit 1
+$TEMP_DIR/ubikom-cli bc register name "$$$" --key=$TEMP_DIR/key1 --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL && exit 1
+$TEMP_DIR/ubikom-cli bc register name "_" --key=$TEMP_DIR/key1 --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL && exit 1
+$TEMP_DIR/ubikom-cli bc register name "-foo" --key=$TEMP_DIR/key1 --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL && exit 1
 
 # Register a name.
-$TEMP_DIR/ubikom-cli bc register name foo --key=$TEMP_DIR/key --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
+$TEMP_DIR/ubikom-cli bc register name foo --key=$TEMP_DIR/key1 --pub-key=$TEMP_DIR/pub_key --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
 
 # Lookup the name.
 $TEMP_DIR/ubikom-cli bc lookup name foo --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
 
 # Update the public key.
 $TEMP_DIR/ubikom-cli create key --out=$TEMP_DIR/pub_key1 --skip-passphrase || exit 1
-$TEMP_DIR/ubikom-cli bc update public-key foo --key=$TEMP_DIR/key --pub-key=$TEMP_DIR/pub_key1 --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
+$TEMP_DIR/ubikom-cli bc update public-key foo --key=$TEMP_DIR/key1 --pub-key=$TEMP_DIR/pub_key1 --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
 
-# Update ownership
-$TEMP_DIR/ubikom-cli bc update owner foo --key=$TEMP_DIR/key --new-owner-address=$2 --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
-
-# Create a connector.
-
-# Lookup the connector.
+# Update ownership.
+$TEMP_DIR/ubikom-cli bc update owner foo --key=$TEMP_DIR/key1 --new-owner-address=$ADDRESS2 --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
 
 # Update price.
-
-# Buy.
-
-# Transfer ownership.
+$TEMP_DIR/ubikom-cli bc update price foo --key=$TEMP_DIR/key2 --price=123456789 --contract-address=$CONTRACT_ADDRESS --node-url=$NODE_URL || exit 1
 
 rm -rf $TEMP_DIR
