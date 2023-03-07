236383dce13e01c88d0c109e6340a7469a5f7f0e
Leonid Gorkin <regnull@gmail.com>
2023-03-07 09:09:04 -0500

    cleanup ubikom-web

--- cmd/ubikom-web/main.go
+++ cmd/ubikom-web/main.go
@@ -9,11 +9,9 @@ import (
 	"os"
 	"time"
 
-	"github.com/dchest/captcha"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/regnull/easyecc"
 	"github.com/regnull/ubikom/bc"
-	"github.com/regnull/ubikom/event"
 	"github.com/regnull/ubikom/globals"
 	"github.com/regnull/ubikom/pb"
 	"github.com/regnull/ubikom/util"
@@ -70,8 +68,6 @@ type CmdArgs struct {
 }
 
 type Server struct {
-	lookupClient          pb.LookupServiceClient
-	identityClient        pb.IdentityServiceClient
 	proxyManagementClient pb.ProxyServiceClient
 	keys                  map[string][]byte
 	privateKey            *easyecc.PrivateKey
@@ -79,18 +75,14 @@ type Server struct {
 	notificationName      string
 	powStrength           int
 	rateLimiter           *rate.Limiter
-	eventSender           *event.Sender
 	blockchain            *bc.Blockchain
 	welcomeMessageDir     string
 }
 
-func NewServer(lookupClient pb.LookupServiceClient, identityClient pb.IdentityServiceClient,
-	proxyManagementClient pb.ProxyServiceClient,
+func NewServer(proxyManagementClient pb.ProxyServiceClient,
 	privateKey *easyecc.PrivateKey, name string, notificationName string, powStrength int,
 	rateLimitPerHour int, blockhain *bc.Blockchain, welcomeMessageDir string) *Server {
 	return &Server{
-		lookupClient:          lookupClient,
-		identityClient:        identityClient,
 		proxyManagementClient: proxyManagementClient,
 		keys:                  make(map[string][]byte),
 		privateKey:            privateKey,
@@ -98,78 +90,9 @@ func NewServer(lookupClient pb.LookupServiceClient, identityClient pb.IdentitySe
 		notificationName:      notificationName,
 		powStrength:           powStrength,
 		rateLimiter:           rate.NewLimiter(rate.Every(time.Hour), rateLimitPerHour),
-		eventSender:           event.NewSender("ubikom-event-processor", "ubikom-web", "web", privateKey, lookupClient),
 		blockchain:            blockhain,
 		welcomeMessageDir:     welcomeMessageDir,
 	}
-}
-
-func (s *Server) HandleNameLookup(w http.ResponseWriter, r *http.Request) {
-	err := s.eventSender.WebPageServed(r.Context(), "name_lookup", "", "", r.UserAgent())
-	if err != nil {
-		log.Error().Err(err).Msg("failed to send event")
-	}
-	name := r.URL.Query().Get("name")
-	_, err = s.lookupClient.LookupName(r.Context(), &pb.LookupNameRequest{
-		Name: name,
-	})
-
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-
-	if !util.ValidateName(name) {
-		log.Warn().Str("name", name).Msg("invalid name")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if err != nil && util.StatusCodeFromError(err) != codes.NotFound {
-		log.Error().Err(err).Msg("name lookup request failed")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	found := err == nil
-
-	w.Header().Add("Content-Type", "application/json")
-
-	// If we got here, the name record was found.
-	fmt.Fprintf(w, `{
-	"name": "%s", 
-	"available": %v
-}`, name, !found)
-}
-
-type EasySetupRequest struct {
-	Name            string `json:"name"`
-	Password        string `json:"password"`
-	EmailKeyOnly    bool   `json:"email_key_only"`
-	CaptchaId       string `json:"captcha_id"`
-	CaptchaSolution string `json:"captcha_solution"`
-	Language        string `json:"language"`
-}
-
-func (s *Server) HandleEasySetup(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "OPTIONS" {
-		// This is a "pre-flight" request, see https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Methods", "POST, GET")
-		w.Header().Add("Access-Control-Allow-Headers", "*")
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	if !s.rateLimiter.Allow() {
-		log.Warn().Msg("rate limit triggered")
-		w.WriteHeader(http.StatusTooManyRequests)
-		return
-	}
-
-	// !!!!! Registration disabled.
-	log.Warn().Msg("registration disabled")
-	w.WriteHeader(http.StatusServiceUnavailable)
-	return
-
 }
 
 type ChangePasswordRequest struct {
@@ -260,30 +183,6 @@ func (s *Server) HandleGetKey(w http.ResponseWriter, r *http.Request) {
 	w.Write(key)
 	// TODO: Remove this after the testing is done.
 	// delete(s.keys, keyID)
-}
-
-func (s *Server) HandleNewCaptcha(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "OPTIONS" {
-		// This is a "pre-flight" request, see https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-		w.Header().Add("Access-Control-Allow-Origin", "*")
-		w.Header().Add("Access-Control-Allow-Methods", "POST, GET")
-		w.Header().Add("Access-Control-Allow-Headers", "*")
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	w.Header().Add("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Content-Type", "application/json")
-	id := captcha.New()
-	fmt.Fprintf(w, `{
-		"id": "%s"
-}`, id)
-	log.Debug().Str("id", id).Msg("processed new captcha request")
-
-	err := s.eventSender.WebPageServed(r.Context(), "new_captcha", "", "", r.UserAgent())
-	if err != nil {
-		log.Error().Err(err).Msg("failed to send event")
-	}
 }
 
 type CheckMailboxKeyRequest struct {
@@ -386,23 +285,9 @@ func main() {
 		grpc.WithTimeout(args.Timeout),
 	}
 
+	var err error
+
 	log.Info().Str("url", args.LookupServiceURL).Msg("connecting to lookup service")
-	lookupConn, err := grpc.Dial(args.LookupServiceURL, opts...)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to connect to the lookup service")
-	}
-	defer lookupConn.Close()
-
-	lookupClient := pb.NewLookupServiceClient(lookupConn)
-
-	log.Info().Str("url", args.IdentityServiceURL).Msg("connecting to identity service")
-	identityConn, err := grpc.Dial(args.IdentityServiceURL, opts...)
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to connect to the identity service")
-	}
-	defer lookupConn.Close()
-
-	identityClient := pb.NewIdentityServiceClient(identityConn)
 
 	var proxyManagementClient pb.ProxyServiceClient
 
@@ -439,24 +324,12 @@ func main() {
 		log.Error().Err(err).Msg("cannot connect to blockchain")
 	}
 
-	var combinedLookupClient pb.LookupServiceClient
-	if args.UseLegacyLookupService {
-		log.Info().Msg("using legacy lookup service")
-		combinedLookupClient = lookupClient
-	} else {
-		combinedLookupClient = bc.NewLookupServiceClient(blockchain, lookupClient, false)
-	}
-
-	server := NewServer(combinedLookupClient, identityClient, proxyManagementClient, privateKey, args.UbikomName,
+	server := NewServer(proxyManagementClient, privateKey, args.UbikomName,
 		args.NotificationName, args.PowStrength, args.RateLimitPerHour, blockchain, args.WelcomeMessageDir)
 
-	http.HandleFunc("/lookupName", server.HandleNameLookup)
-	http.HandleFunc("/easySetup", server.HandleEasySetup)
 	http.HandleFunc("/getKey", server.HandleGetKey)
 	http.HandleFunc("/changePassword", server.HandleChangePassword)
-	http.HandleFunc("/new_captcha", server.HandleNewCaptcha)
 	http.HandleFunc("/check_mailbox_key", server.HandleCheckMailboxKey)
-	http.Handle("/captcha/", captcha.Server(captcha.StdWidth, captcha.StdHeight))
 	log.Info().Int("port", args.Port).Msg("listening...")
 
 	if args.CertFile != "" && args.KeyFile != "" {
