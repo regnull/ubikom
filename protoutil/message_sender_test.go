05adb7d2e2edcb0faf48d0e6c99d1ea61f069d77
Leonid Gorkin <regnull@gmail.com>
2023-08-19 22:07:56 -0400

    great expectations

--- protoutil/message_sender_test.go
+++ protoutil/message_sender_test.go
@@ -12,6 +12,7 @@ import (
 	pumocks "github.com/regnull/ubikom/protoutil/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"google.golang.org/grpc"
 )
 
 func Test_MessageSender(t *testing.T) {
@@ -32,12 +33,22 @@ func Test_MessageSender(t *testing.T) {
 		easyecc.P256).Return(receiverPrivateKey.PublicKey(), nil)
 	bchain.EXPECT().Endpoint(ctx, "bob").Return("bob's endpoint", nil)
 	dscfactory.EXPECT().CreateDumpServiceClient(ctx, "bob's endpoint", time.Duration(0)).Return(dsclient, nil, nil)
-	// TODO: A better expectation here.
-	dsclient.EXPECT().Send(ctx, mock.Anything).Return(&pb.SendResponse{}, nil)
+	dsclient.EXPECT().Send(ctx, mock.Anything).RunAndReturn(
+		func(ctx context.Context, req *pb.SendRequest,
+			opts ...grpc.CallOption) (*pb.SendResponse, error) {
+			assert.Equal("alice", req.GetMessage().GetSender())
+			assert.Equal("bob", req.GetMessage().GetReceiver())
+			assert.True(VerifySignature(req.GetMessage().GetSignature(), privateKey.PublicKey(), req.GetMessage().GetContent()))
+			content, err := receiverPrivateKey.Decrypt(req.GetMessage().GetContent(), privateKey.PublicKey())
+			assert.NoError(err)
+			assert.Equal("the message", string(content))
+			return &pb.SendResponse{}, nil
+		})
 	sender := NewMessageSender(dscfactory, bchain)
 	err = sender.Send(ctx, privateKey, []byte("the message"), "alice", "bob")
 	assert.NoError(err)
 
 	bchain.AssertExpectations(t)
 	dscfactory.AssertExpectations(t)
+	dsclient.AssertExpectations(t)
 }
