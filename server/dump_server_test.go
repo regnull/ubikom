f5b611d7e11f8f99a6f9269a6e9bc9ad8b062d6b
Leonid Gorkin <regnull@gmail.com>
2023-09-06 09:33:53 -0400

    add test

--- server/dump_server_test.go
+++ server/dump_server_test.go
@@ -60,3 +60,53 @@ func Test_DumpServer_SendReceive(t *testing.T) {
 
 	bchain.AssertExpectations(t)
 }
+
+func Test_DumpServer_IdentityFallback(t *testing.T) {
+	// This test should go away after the identity proof is working as expected.
+	assert := assert.New(t)
+
+	dumpStore := store.NewMemory()
+	bchain := new(bcmocks.MockBlockchain)
+	ctx := context.Background()
+	dumpServer := NewDumpServer(dumpStore, bchain)
+
+	aliceKey, err := easyecc.NewPrivateKey(easyecc.P256)
+	assert.NoError(err)
+	bobKey, err := easyecc.NewPrivateKey(easyecc.P256)
+	assert.NoError(err)
+
+	bchain.EXPECT().PublicKeyByCurve(ctx, "alice",
+		easyecc.P256).Return(aliceKey.PublicKey(), nil)
+	bchain.EXPECT().PublicKeyByCurve(ctx, "bob",
+		easyecc.P256).Return(bobKey.PublicKey(), nil)
+
+	msg, err := protoutil.CreateMessage(aliceKey, []byte("hi bob"), "alice", "bob", bobKey.PublicKey())
+	assert.NoError(err)
+
+	sendRes, err := dumpServer.Send(ctx, &pb.SendRequest{Message: msg})
+	assert.NoError(err)
+	assert.NotNil(sendRes)
+
+	// Intentionally send old proof of identity.
+	identityProof, err := protoutil.IdentityProof(bobKey, time.Now().Add(-time.Second*100))
+	assert.NoError(err)
+	req := &pb.ReceiveRequest{
+		IdentityProof: identityProof,
+		CryptoContext: &pb.CryptoContext{
+			EllipticCurve: pb.EllipticCurve(easyecc.P256),
+			EcdhVersion:   2,
+			EcdsaVersion:  1,
+		},
+	}
+	receiveRes, err := dumpServer.Receive(ctx, req)
+	assert.NoError(err)
+	assert.NotNil(receiveRes)
+	assert.Equal("alice", receiveRes.GetMessage().GetSender())
+	assert.Equal("bob", receiveRes.GetMessage().GetReceiver())
+
+	content, err := protoutil.DecryptMessage(ctx, bchain, bobKey, receiveRes.GetMessage())
+	assert.NoError(err)
+	assert.Equal("hi bob", content)
+
+	bchain.AssertExpectations(t)
+}
